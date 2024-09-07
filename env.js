5552dbe73c9b851928e1bf678aea34158daff920
Pratham Upadhyay <63505631+PRATHAMU200@users.noreply.github.com>
2024-09-07 22:33:07 +0530

    Delete env.js

--- env.js
+++
@@ -1,8 +0,0 @@
-const env = {
-  // Add your environment variables here
-  API_URL: "upi://pay?pa=prathamu200@oksbi&pn=Pratham%20Upadhyay&cu=INR",
-  DISCORD_WEBHOOK_API:
-    "https://discord.com/api/webhooks/1275149874433626262/lyAWWu599pvsXUMexlx3ijjlPSj8_sUIZl5BtkJvFccPqJO5KsNC3JDg6G5ZL0cO1kf7",
-};
-
-export default env;
